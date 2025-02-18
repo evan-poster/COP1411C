7094f8e317077fb38bcc6a059d054085b40546c6
evan-poster <evan.poster@gmail.com>
2025-02-18 09:19:33 -0500

    Week 1/Ex1 updated for StringBuilder() requirement

--- Week 1/Exercise_1.java
+++ Week 1/Exercise_1.java
@@ -1,14 +1,19 @@
 // Reimplement 1.1.9, without using Integer.toBinaryString
 
+// Update 1: Using StringBuilder per instructions
+
+import java.lang.StringBuilder;
+
 public class Exercise_1 {
 	public static void main(String[] args) {
 		int N = 10;
-		String s = "";
+		StringBuilder s = new StringBuilder();
+		StringBuilder stringBuilder = new StringBuilder();
 		for (int n = N; n > 0; n /= 2) {
 			System.out.println("Current n: " + n);
-			s = (n % 2 == 0 ? "0" : "1") + s;
-			System.out.println("Current binary string: " + s);
+			stringBuilder.insert(0, (n % 2 == 0 ? "0" : "1"));
+			System.out.println("Current binary string: " + stringBuilder.toString());
 		}
-		System.out.println("Final binary string: " + s);
+		System.out.println("Final binary string: " + stringBuilder.toString());
 	}
 }
